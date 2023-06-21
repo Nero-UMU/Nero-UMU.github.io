c3b27cf0b46fcc04cefada1fb5b07f84f02fcd5b
Nero-UMU <2572591652@qq.com>
2023-06-21 22:19:22 +0800

    Site updated: 2023-06-21 22:19:21

--- js/sakana.js
+++ js/sakana.js
@@ -1,3 +1,3 @@
 !(function() {
-    new SakanaWidget({autoFit: true, controls: false, character: 'takina'}).mount('#sakana-widget');
+    new SakanaWidget({autoFit: true, controls: true, character: 'takina'}).mount('#sakana-widget');
 })();
